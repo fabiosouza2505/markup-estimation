3b4fdc0415e8e1102acee8311407eea2d5f5310e
fabiosouza2505 <fabiodemedeirossouza@gmail.com>
2026-02-23 09:48:47 -0300

    pasta 8sect

--- code/estimation-8sect/truncate_str.m
+++
@@ -1,8 +0,0 @@
-function str_trunc = truncate_str(str, max_len)
-    %TRUNCATE_STR Trunca string para comprimento máximo
-    if length(str) > max_len
-        str_trunc = [str(1:max_len-3) '...'];
-    else
-        str_trunc = str;
-    end
-end
